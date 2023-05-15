ce7b861f88093cc89a83d29e3bcb87f9fbd8e745
Liliia Baieva <555blv@gmail.com>
2023-05-15 18:51:32 +0300

    add checkbox, some change style of "ul"

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const task = document.getElementById('taskInput');
 const addTaskButton = document.getElementById('addTask');
-const taskListOl = document.getElementById('taskList');
+const taskListul = document.getElementById('taskList');
+const checkbox = document.getElementById('checkbox');
 
 addTaskButton.addEventListener('click', addTask);
-taskListOl.addEventListener('click', (e) => {
-    // console.log(e.target.tagName);
+taskListul.addEventListener('click', (e) => {
+    // console.log(e.target);
     if(e.target.tagName === 'BUTTON'){
         e.target.parentElement.remove();
     }
+    if(e.target.checked){
+        // console.log('checked');
+        e.target.parentElement.style = 'text-decoration: line-through';
+    } else {
+        e.target.parentElement.style = !'text-decoration: line-through';
+    }
+
 });
 
 function addTask(){
     const taskName = task.value.trim();
     if(taskName){
         const li = document.createElement('li');
-        li.innerHTML = `<span>${taskName}  </span><button>Delete</button>`;
-        taskListOl.appendChild(li);
+        li.innerHTML = `<input type="checkbox" id="checkbox"><span>${taskName}</span><button>Delete</button>`;
+        taskListul.appendChild(li);
     }
     task.value = '';
 }
+
+
+
+
 
